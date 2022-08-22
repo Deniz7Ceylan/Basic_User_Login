a71f281593f0282fb202cebdb44f222b681fcbc5
Deniz7Ceylan <dc7info@gmail.com>
2022-08-23 02:56:54 +0300

    B_U_L_Sample
    Finally

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string id;
+            int pw;
+            do
+            {
+                Console.Write("Kullanıcı adınızı giriniz: ");
+                id = Console.ReadLine();
+                if (id == "KingsMan")
+                {
+                    Console.Write("Şifrenizi giriniz: ");
+                    pw = Convert.ToInt32(Console.ReadLine());
+                    if (pw == 1234)
+                    {
+                        Console.WriteLine("Giriş Başarılı! Hoşgeldin Ajan.");
+                    }
+                    else
+                        Console.WriteLine("Şifre hatalı, imha için geri sayım başlıyor...");
+                }
+
+            } while (id != "KingsMan");
+        }
+        
+    }
+}
